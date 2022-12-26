06cec3edaa9ce70cd736dff1ef7374f3317b2eec
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-26 15:58:13 -0500

    test: updated test cases

--- tests/unittests/test_main.py
+++ tests/unittests/test_main.py
@@ -2,14 +2,14 @@ from english_detector.main import detect_english
 
 
 class TestMain:
+    def __init__(self: 'TestMain') -> None:
+        self.text = 'Medium is a social publishing platform with a diverse array of metaverse and gaming-related articles and stories. View this article at medium.com.'
+
     def test_detect_english_return_value(self: 'TestMain') -> None:
-        assert detect_english('this is english')
+        assert detect_english(self.text)
 
     def test_detect_english_return_type(self: 'TestMain') -> None:
-        assert isinstance(detect_english('this is english'), bool)
-
-    def test_detect_english_with_score(self: 'TestMain') -> None:
-        assert detect_english('this is english', 0.98)
+        assert isinstance(detect_english(self.text), bool)
 
     def test_detect_english_no_text(self: 'TestMain') -> None:
         assert detect_english(None)
