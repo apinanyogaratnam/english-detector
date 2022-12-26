35456f68ff01e8323f9ef3a3c666240a27edfbbb
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-26 15:51:42 -0500

    fix: getting a valid response

--- english_detector/main.py
+++ english_detector/main.py
@@ -1,17 +1,9 @@
 import pycld2 as cld2
-from textblob import TextBlob
 
 
 def detect_english(text: str | None) -> bool:
     if not text:
         return True
 
-    return TextBlob(text).detect_language()
-
     _, _, _, detect_language = cld2.detect(text, returnVectors=True)
-
     return len(detect_language) == 1 and detect_language[0][2] == 'ENGLISH'
-
-
-if __name__ == '__main__':
-    print(detect_english('this is english.'))
