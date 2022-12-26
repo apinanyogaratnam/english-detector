da0466d6dfaed3f64e5ce818eeb13ff93a739bbe
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-26 15:03:20 -0500

    test: test cases

--- tests/unittests/test_main.py
+++ tests/unittests/test_main.py
@@ -1,9 +1,12 @@
-from english_detector.main import greetings
+from english_detector.main import detect_english
 
 
 class TestMain:
-    def test_greetings_return_value(self):
-        assert greetings() == "Hello, World!"
+    def test_detect_english_return_value(self: 'TestMain') -> None:
+        assert detect_english('this is english')
 
-    def test_greetings_return_type(self):
-        assert isinstance(greetings(), str)
+    def test_detect_english_return_type(self: 'TestMain') -> None:
+        assert isinstance(detect_english('this is english'), bool)
+
+    def test_detect_english_with_score(self: 'TestMain') -> None:
+        assert detect_english('this is english', 0.98)
